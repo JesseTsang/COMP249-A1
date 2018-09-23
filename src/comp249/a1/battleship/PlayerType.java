c48d9b0f86428003ed467501dc5716e5fce36a2c
Jesse Tsang <JesseTsang@outlook.com>
2018-09-23 19:51:40 -0400

    Add new enum class to classify player type.

---
+++ src/comp249/a1/battleship/PlayerType.java
@@ -0,0 +1,14 @@
+package comp249.a1.battleship;
+
+/**
+* <h1>The PlayerType enum class provides classification for player type.</h1>
+*
+* @author  Jesse Tsang
+* @version 1.0
+* @since   23th September 2018
+*/
+public enum PlayerType 
+{
+	HUMAN,
+	COMPUTER;
+}
